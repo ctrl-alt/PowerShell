8caf1e3c01ed5d656387f1534f0edb1a311cdff2
Victor <ourlapov@mail.ru>
2018-12-06 01:06:44 +0300

    закоментировал $date = Get-Date -format yyyy-MM-dd

--- ping1.ps1
+++ ping1.ps1
@@ -1,6 +1,6 @@
 ﻿param($h='8.8.8.8') #Входящий параметр, указываем какой хост пинговать, например: ping.ps1 -h 192.168.1.1
 $ping = new-object System.Net.NetworkInformation.Ping
-$date = Get-Date -format yyyy-MM-dd
+#$date = Get-Date -format yyyy-MM-dd
 While (1 -ne 0) { #бесконечный цикл
  $reply = $ping.send($h)
  $timestamp = "{0:yyyy-MM-dd HH:mm:ss}" -f (get-date) #отметка времени
